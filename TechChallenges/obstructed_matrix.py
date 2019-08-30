613a2b7d56540f5f8a58e64cab0c83bb123c1435
Jacob <jbmenashi@gmail.com>
2019-08-30 11:14:42 -0400

    finished matrix

--- TechChallenges/obstructed_matrix.py
+++ TechChallenges/obstructed_matrix.py
@@ -2,4 +2,18 @@
 
 def uniquePathsWithObstacles(matrix):
    paths = [[0]*len(matrix[0] for i in A)]
-   
+   if matrix[0][0] == 0:
+      paths[0][0] = 1
+
+   for i in range(1, len(matrix)):
+      if matrix[i][0] == 0:
+         paths[i][0] = paths[i-1][0]
+
+   for j in range(1, len(matrix[0])):
+      if matrix[0][j] == 0:
+         paths[0][j] = paths[0][j-1]
+
+   for i in range(1, len[matrix]):
+      for j in range(1, len(matrix[0])):
+         if matrix[i][j] == 0:
+            paths[i][j] = paths[i-1][j] + paths[i][j-1]
