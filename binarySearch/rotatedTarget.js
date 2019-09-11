29a161faf60ec24094506bb077259a86d8769842
Jacob <jbmenashi@gmail.com>
2019-09-11 12:46:49 -0400

    rotated solutions

---
+++ binarySearch/rotatedTarget.js
@@ -0,0 +1,59 @@
+// log n solution
+
+var search = function (nums, target) {
+   let left = 0;
+   let right = nums.length - 1;
+   while (left <= right) {
+      let mid = Math.floor((left + right) / 2);
+      if (nums[mid] === target) {
+         return mid;
+      }
+      // When dividing the roated array into two halves, one must be sorted.
+      // Check if the left side is sorted
+      if (nums[left] < nums[mid]) {
+         if (nums[left] < target && target < nums[mid]) {
+            // target is in the left
+            right = mid - 1;
+         } else {
+            // target is in the right
+            left = mid + 1;
+         }
+      }
+      // Otherwise, the right side is sorted
+      else {
+         if (nums[mid] < target && target < nums[right]) {
+            // target is in the right
+            left = mid + 1;
+         } else {
+            // target is in the left
+            right = mid - 1;
+         }
+      }
+   }
+   return -1;
+};
+
+// other solution
+
+var search = function (nums, target) {
+   let sorted = [...nums].sort((a, b) => a - b)
+   let l = 0
+   let r = nums.length - 1
+   while (l + 1 < r) {
+      let mid = Math.floor((l + r) / 2)
+      if (sorted[mid] === target) {
+         return nums.indexOf(sorted[mid])
+      } else if (sorted[mid] < target) {
+         l = mid + 1
+      } else {
+         r = mid - 1
+      }
+   }
+   if (sorted[l] === target) {
+      return nums.indexOf(sorted[l])
+   } else if (sorted[r] === target) {
+      return nums.indexOf(sorted[r])
+   } else {
+      return -1
+   }
+};
