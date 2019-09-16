4f21669d65adb648bc8f283bf963f83b7a80ad1e
Jacob <jbmenashi@gmail.com>
2019-09-16 10:53:48 -0400

    pre in post order traversals

--- binaryTree/treeTraversal.py
+++ binaryTree/treeTraversal.py
@@ -14,4 +14,26 @@ def traverse(self):
 # but for trees, you have to it either recursively or iteratively
 # or breadth first search / depth first search? I don't get it
 
+# Pre-Order Traversal for DFS - # A B D E C F G
+def preTraversal(root):
+   if not root:
+      return
+   print(root.val) # prints the value
+   preTraversal(root.left) # recursive, goes all the way down the left side
+   preTraversal(root.right) # then the right side when there's no more lefts
 
+#In-Order Traversal - # D B E A F C G
+def inTraversal(root): 
+   if not root:
+      return
+   inTraversal(root.left) # so this goes all the way down the left before printing the first one
+   print(root.val)
+   inTraversal(root.right)  
+
+#Post-Order Traversal - # D E B F G C A
+def inTraversal(root): 
+   if not root:
+      return
+   inTraversal(root.left)
+   inTraversal(root.right)  
+   print(root.val)
