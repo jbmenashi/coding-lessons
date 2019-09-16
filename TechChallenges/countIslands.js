e8d9c569515b85fb867e5fd0428f2f4d2fe2e592
Jacob <jbmenashi@gmail.com>
2019-09-16 14:33:51 -0400

    counting islands

---
+++ TechChallenges/countIslands.js
@@ -0,0 +1,38 @@
+function countIslands(mat) {
+   let count = 0
+   let rows = mat.length
+   let cols = mat[0].length
+   for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < cols; j++) {
+         if (mat[i][j] === 1) {
+            checkIsland(i, j, mat)
+            count++
+         }
+      }
+   }
+   return count
+}
+
+function checkIsland(row, col, mat) {
+   if (!mat[row] || !mat[row][col]) {
+      return
+   }
+   if (mat[row][col] !== 1) {
+      return
+   }
+
+   mat[row][col] = 'x'
+   checkIsland(row, col + 1, mat) //right
+   checkIsland(row, col - 1, mat) //left
+   checkIsland(row + 1, col, mat) //top
+   checkIsland(row - 1, col, mat) //bottom
+}
+
+
+countIslands([
+   [1, 1, 0, 0, 0],
+   [0, 1, 0, 0, 1],
+   [1, 0, 0, 1, 1],
+   [0, 0, 0, 0, 0],
+   [1, 0, 1, 0, 1]
+])
